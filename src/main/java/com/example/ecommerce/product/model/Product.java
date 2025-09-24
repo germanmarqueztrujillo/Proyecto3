e10ed26a7e6129c54fb439b4c7a3ae8dc96a4bc9
GermanMT <germanoctako@gmail.com>
2025-09-24 15:59:09 +0200

    feat: add product model

---
+++ src/main/java/com/example/ecommerce/product/model/Product.java
@@ -0,0 +1,43 @@
+package com.example.ecommerce.product.model;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.ecommerce.order.model.Order;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @NotBlank
+  @Column(nullable = false)
+  private String name;
+
+  @NotNull
+  @Positive
+  private BigDecimal price;
+
+  @ManyToMany(mappedBy = "products")
+  private List<Order> students = new ArrayList<Order>();
+}
