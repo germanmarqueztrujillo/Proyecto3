c95738a209d83956d4a918a8f3a2e2efadee117f
GermanMT <germanoctako@gmail.com>
2025-09-24 16:19:21 +0200

    feat: implement create order service

--- src/main/java/com/example/ecommerce/order/service/OrderService.java
+++ src/main/java/com/example/ecommerce/order/service/OrderService.java
@@ -3,6 +3,9 @@ package com.example.ecommerce.order.service;
 import org.springframework.stereotype.Service;
 
 import com.example.ecommerce.customer.repository.CustomerRepository;
+import com.example.ecommerce.order.dto.OrderDTO;
+import com.example.ecommerce.order.model.Order;
+import com.example.ecommerce.order.mapper.OrderMapper;
 import com.example.ecommerce.order.repository.OrderRepository;
 import com.example.ecommerce.product.repository.ProductRepository;
 
@@ -12,14 +15,21 @@ public class OrderService {
   private final OrderRepository orderRepository;
   private final CustomerRepository customerRepository;
   private final ProductRepository productRepository;
+  private final OrderMapper orderMapper;
 
   public OrderService(
       OrderRepository orderRepository,
       CustomerRepository customerRepository,
-      ProductRepository productRepository) {
+      ProductRepository productRepository,
+      OrderMapper orderMapper) {
     this.orderRepository = orderRepository;
     this.customerRepository = customerRepository;
     this.productRepository = productRepository;
+    this.orderMapper = orderMapper;
   }
   
+  public OrderDTO createOrder(OrderDTO orderDto) {
+    Order createdOrder = orderRepository.save(orderMapper.toEntity(orderDto));
+    return orderMapper.toDTO(createdOrder);
+  }
 }
