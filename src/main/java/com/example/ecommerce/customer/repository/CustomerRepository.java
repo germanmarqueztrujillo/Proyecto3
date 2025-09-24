4e720a29c7d41bcb7046f9e1d6176408b7cbdeac
GermanMT <germanoctako@gmail.com>
2025-09-24 16:04:14 +0200

    feat: add customer repository

---
+++ src/main/java/com/example/ecommerce/customer/repository/CustomerRepository.java
@@ -0,0 +1,10 @@
+package com.example.ecommerce.customer.repository;
+
+import com.example.ecommerce.customer.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+  
+}
