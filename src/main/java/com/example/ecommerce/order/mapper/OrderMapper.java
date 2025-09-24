c4c6eef373188ff482e8bd9c2313f47a80aae83f
GermanMT <germanoctako@gmail.com>
2025-09-24 16:50:45 +0200

    feat: use order create dto for transforming in to an entityç

--- src/main/java/com/example/ecommerce/order/mapper/OrderMapper.java
+++ src/main/java/com/example/ecommerce/order/mapper/OrderMapper.java
@@ -1,5 +1,6 @@
 package com.example.ecommerce.order.mapper;
 
+import com.example.ecommerce.order.dto.OrderCreateDTO;
 import com.example.ecommerce.order.dto.OrderDTO;
 import com.example.ecommerce.order.model.Order;
 import com.example.ecommerce.product.model.Product;
@@ -12,9 +13,10 @@ import org.mapstruct.Mapping;
 @Mapper(componentModel = "spring")
 public interface OrderMapper {
   @Mapping(target = "id", ignore = true)
+  @Mapping(target = "status", ignore = true)
   @Mapping(target = "customer", ignore = true)
   @Mapping(target = "products", ignore = true)
-  Order toEntity(OrderDTO dto);
+  Order toEntity(OrderCreateDTO dto);
 
   @Mapping(source = "customer.id", target = "customerId")
   @Mapping(source = "products", target = "productsId")
