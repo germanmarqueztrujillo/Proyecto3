bf223d4755a2e927fc52d6cdadb4d0a0fcb58d22
GermanMT <germanoctako@gmail.com>
2025-09-24 16:54:19 +0200

    feat: add update status to paid service

--- src/main/java/com/example/ecommerce/order/service/OrderService.java
+++ src/main/java/com/example/ecommerce/order/service/OrderService.java
@@ -3,9 +3,13 @@ package com.example.ecommerce.order.service;
 import org.springframework.stereotype.Service;
 
 import com.example.ecommerce.customer.repository.CustomerRepository;
+import com.example.ecommerce.order.dto.OrderCreateDTO;
 import com.example.ecommerce.order.dto.OrderDTO;
 import com.example.ecommerce.order.exception.EmptyProductsException;
+import com.example.ecommerce.order.exception.OrderNotFoundException;
+import com.example.ecommerce.order.exception.OrderStatusIsNotCreated;
 import com.example.ecommerce.order.model.Order;
+import com.example.ecommerce.order.model.Status;
 import com.example.ecommerce.order.mapper.OrderMapper;
 import com.example.ecommerce.order.repository.OrderRepository;
 import com.example.ecommerce.product.repository.ProductRepository;
@@ -29,11 +33,23 @@ public class OrderService {
     this.orderMapper = orderMapper;
   }
   
-  public OrderDTO createOrder(OrderDTO orderDto) {
-    if (orderDto.getProductsId().size() == 0) {
+  public OrderDTO createOrder(OrderCreateDTO orderCreateDto) {
+    if (orderCreateDto.getProductsId().size() == 0) {
       throw new EmptyProductsException();
     }
-    Order createdOrder = orderRepository.save(orderMapper.toEntity(orderDto));
+    Order createdOrder = orderRepository.save(orderMapper.toEntity(orderCreateDto));
     return orderMapper.toDTO(createdOrder);
+  }
+
+  public void updateStatusToPaid(Long orderId) {
+    Order order = orderRepository.findById(orderId).orElseThrow(() -> new OrderNotFoundException(orderId));;
+
+    if (order.getProducts().size() == 0) {
+      throw new EmptyProductsException();
+    } else if (order.getStatus() != Status.CREATED) {
+      throw new OrderStatusIsNotCreated();
+    }
+
+    orderRepository.updateOrderStatusById(orderId);
   }
 }
