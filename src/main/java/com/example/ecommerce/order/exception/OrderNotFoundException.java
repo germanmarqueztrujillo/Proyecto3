7ba9212731725778efc88ed0c86e85e9e69e91a5
GermanMT <germanoctako@gmail.com>
2025-09-24 16:55:05 +0200

    feat: implement order not found exception

---
+++ src/main/java/com/example/ecommerce/order/exception/OrderNotFoundException.java
@@ -0,0 +1,11 @@
+package com.example.ecommerce.order.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class OrderNotFoundException extends RuntimeException {
+  public OrderNotFoundException(Long orderId) {
+    super("Order with id " + orderId + " not found");
+  }
+}
