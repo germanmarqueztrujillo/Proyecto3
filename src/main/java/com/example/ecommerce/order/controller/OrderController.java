b2ea1af3ace17b87ede2cd8ace044e06141f87d0
GermanMT <germanoctako@gmail.com>
2025-09-24 17:07:49 +0200

    feat: impletement create and get by id orders endpoints

---
+++ src/main/java/com/example/ecommerce/order/controller/OrderController.java
@@ -0,0 +1,32 @@
+package com.example.ecommerce.order.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.ecommerce.order.dto.OrderCreateDTO;
+import com.example.ecommerce.order.dto.OrderDTO;
+import com.example.ecommerce.order.service.OrderService;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+
+  private final OrderService orderService;
+
+  public OrderController(OrderService orderService) {
+    this.orderService = orderService;
+  }
+
+  @PostMapping
+  public OrderDTO createOrder(OrderCreateDTO orderCreateDTO) {
+    return orderService.createOrder(orderCreateDTO);
+  }
+
+  @GetMapping("/{id}")
+  public OrderDTO getOrder(@PathVariable Long id) {
+    return orderService.getOrderById(id);
+  }
+}
--- src/main/java/com/example/ecommerce/order/service/OrderService.java
+++ src/main/java/com/example/ecommerce/order/service/OrderService.java
@@ -1,6 +1,5 @@
 package com.example.ecommerce.order.service;
 
-import com.example.ecommerce.customer.repository.CustomerRepository;
 import com.example.ecommerce.order.dto.OrderCreateDTO;
 import com.example.ecommerce.order.dto.OrderDTO;
 import com.example.ecommerce.order.exception.EmptyProductsException;
@@ -10,26 +9,26 @@ import com.example.ecommerce.order.mapper.OrderMapper;
 import com.example.ecommerce.order.model.Order;
 import com.example.ecommerce.order.model.Status;
 import com.example.ecommerce.order.repository.OrderRepository;
-import com.example.ecommerce.product.repository.ProductRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderService {
 
   private final OrderRepository orderRepository;
-  private final CustomerRepository customerRepository;
-  private final ProductRepository productRepository;
   private final OrderMapper orderMapper;
 
   public OrderService(
       OrderRepository orderRepository,
-      CustomerRepository customerRepository,
-      ProductRepository productRepository,
       OrderMapper orderMapper) {
     this.orderRepository = orderRepository;
-    this.customerRepository = customerRepository;
-    this.productRepository = productRepository;
     this.orderMapper = orderMapper;
+  }
+
+  public OrderDTO getOrderById(Long orderId) {
+    Order order =
+        orderRepository.findById(orderId).orElseThrow(() -> new OrderNotFoundException(orderId));
+    ;
+    return orderMapper.toDTO(order);
   }
 
   public OrderDTO createOrder(OrderCreateDTO orderCreateDto) {
