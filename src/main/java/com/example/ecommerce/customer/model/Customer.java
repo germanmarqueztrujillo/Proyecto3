eb85f939411dabcc91638e92bebc3cd22c518fb1
GermanMT <germanoctako@gmail.com>
2025-09-24 15:57:58 +0200

    feat: add customer model

---
+++ src/main/java/com/example/ecommerce/customer/model/Customer.java
@@ -0,0 +1,41 @@
+package com.example.ecommerce.customer.model;
+
+import java.util.List;
+
+import com.example.ecommerce.order.model.Order;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @NotBlank
+  @Column(nullable = false)
+  private String name;
+
+  @NotBlank
+  @Column(nullable = false, unique = true)
+  @Email
+  private String email;
+
+  @OneToMany(mappedBy = "customer")
+  private List<Order> orders;
+}
